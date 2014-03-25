79f2cddb7433746af0984e3b80bc266588d8d372
Masahito Yoshida <masahito@axsh.net>
2014-03-25 19:24:21 +0900

    enable to set fcfg_path with IFCFG_PATH_PREFIX.

--- test/unit/ifcfg-setup/t.gen_ifcfg_path.sh
+++ test/unit/ifcfg-setup/t.gen_ifcfg_path.sh
@@ -31,6 +31,11 @@ function test_gen_ifcfg_path_opts() {
   assertEquals $(gen_ifcfg_path ${ifname}) ${ifcfg_path_prefix}-${ifname}
 }
 
+function test_gen_ifcfg_path_opts_const() {
+  IFCFG_PATH_PREFIX=/path/to/ifcfg
+  assertEquals $(gen_ifcfg_path ${ifname}) ${IFCFG_PATH_PREFIX}-${ifname}
+}
+
 ## shunit2
 
 . ${shunit2_file}
