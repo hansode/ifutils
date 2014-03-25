53e080d25ca361b0bb906bb0db4e84e283335302
Masahito Yoshida <masahito@axsh.net>
2014-03-25 19:58:32 +0900

    add render_ifcfg_bond_slave test.

---
+++ test/unit/ifcfg-setup/t.render_ifcfg_bond_slave.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# requires:
+#   bash
+#
+
+## include files
+
+. $(cd ${BASH_SOURCE[0]%/*} && pwd)/helper_shunit2.sh
+
+## variables
+
+declare ifname=eth0
+
+## functions
+
+function setUp() {
+  :
+}
+
+function tearDown() {
+  :
+}
+
+function test_render_ifcfg_bond_slave_no_opts() {
+  local body="DEVICE=${ifname}
+BOOTPROTO=none
+ONBOOT=yes
+MASTER=${master}
+SLAVE=yes"
+  assertEquals "${body}" "$(render_ifcfg_bond_slave)"
+}
+
+function test_render_ifcfg_bond_slave_opts() {
+  local body="DEVICE=${ifname}
+BOOTPROTO=none
+ONBOOT=yes
+MASTER=${master}
+SLAVE=yes"
+  assertEquals "${body}" "$(render_ifcfg_bond_slave ${ifname})"
+}
+
+## shunit2
+
+. ${shunit2_file}
