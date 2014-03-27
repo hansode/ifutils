6adcd892fa19d0d6750369ccb7e0ca89ef22a550
Masahito Yoshida <masahito@axsh.net>
2014-03-27 23:14:20 +0900

    add configure_vlan_conf into map_ifcfg_vlan.

--- test/unit/ifcfg-setup/t.map_ifcfg_vlan.sh
+++ test/unit/ifcfg-setup/t.map_ifcfg_vlan.sh
@@ -14,19 +14,20 @@
 
 function setUp() {
   function gen_ifcfg_path() { echo /dev/null; }
+  function configure_vlan_conf() { echo configure_vlan_conf; }
 }
 
 function tearDown() {
   :
 }
 
-function test_map_ifcfg_bond_no_opts() {
-  map_ifcfg_bond
+function test_map_ifcfg_vlan_no_opts() {
+  map_ifcfg_vlan
   assertEquals 0 ${?}
 }
 
-function test_map_ifcfg_bond_opts() {
-  map_ifcfg_bond
+function test_map_ifcfg_vlan_opts() {
+  map_ifcfg_vlan
   assertEquals 0 ${?}
 }
 
